feb6c0e1fe43492ea6af81c8861b91b558f47c50
JunJangE <wnswkd486@gmail.com>
2025-06-18 19:26:15 +0900

    feat: Home, Bookmark Screen 연결 (#4)

---
+++ composeApp/src/commonMain/kotlin/linkletter/client/feature/home/HomeScreen.kt
@@ -0,0 +1,24 @@
+package linkletter.client.feature.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier =
+            modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(8.dp),
+    ) {
+        Text(text = "Home")
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/linkletter/client/feature/main/MainScreen.kt
@@ -0,0 +1,45 @@
+package linkletter.client.feature.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import linkletter.client.core.designsystem.theme.LinkletterTheme
+import linkletter.client.feature.main.components.MainBottomBar
+import linkletter.client.feature.main.components.MainNavHost
+import linkletter.client.feature.main.navigation.MainNavigator
+import linkletter.client.feature.main.navigation.rememberMainNavigator
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    MainContent(modifier = modifier)
+}
+
+@Composable
+fun MainContent(
+    modifier: Modifier = Modifier,
+    navigator: MainNavigator = rememberMainNavigator(),
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        containerColor = LinkletterTheme.colorScheme.background,
+        bottomBar = {
+            MainBottomBar(
+                currentTab = navigator.currentTab,
+                onTabSelected = { navigator.navigate(it) },
+            )
+        },
+    ) { innerPadding ->
+        Surface(
+            modifier = modifier.fillMaxSize().padding(innerPadding),
+            color = LinkletterTheme.colorScheme.background,
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                MainNavHost(navigator)
+            }
+        }
+    }
+}
