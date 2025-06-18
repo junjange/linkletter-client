182eaeba5bef4d4a6bb03f52f0d604092c161621
JunJangE <wnswkd486@gmail.com>
2025-06-19 00:46:50 +0900

    feat: HomeSearchBar (#7)

--- composeApp/src/commonMain/kotlin/linkletter/client/feature/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/linkletter/client/feature/home/HomeScreen.kt
@@ -2,13 +2,13 @@ package linkletter.client.feature.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalFocusManager
+import linkletter.client.core.designsystem.utils.addFocusCleaner
+import linkletter.client.feature.home.componets.HomeSearchBar
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -16,13 +16,19 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = koinViewModel(),
 ) {
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier =
             modifier
                 .verticalScroll(rememberScrollState())
                 .fillMaxSize()
-                .padding(8.dp),
+                .addFocusCleaner(focusManager),
     ) {
-        Text(text = "Home")
+        HomeSearchBar(
+            focusManager = focusManager,
+            onSearch = { query ->
+            },
+        )
     }
 }
