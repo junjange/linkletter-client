18cc2beccf41759915c64f4d2a30e705c9e4c849
JunJangE <wnswkd486@gmail.com>
2025-07-03 11:37:26 +0900

    refactor: Blog Follow Screen UI 수정 (#22)

--- feature/blogfollow/src/commonMain/kotlin/linkletter/client/feature/blogfollow/BlogFollowViewModel.kt
+++ feature/blogfollow/src/commonMain/kotlin/linkletter/client/feature/blogfollow/BlogFollowViewModel.kt
@@ -3,37 +3,66 @@ package linkletter.client.feature.blogfollow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import linkletter.client.core.common.formatRssDateToKorean
+import linkletter.client.core.domain.usecase.GetBlogUseCase
 import linkletter.client.feature.blogfollow.model.BlogFollowEffect
 import linkletter.client.feature.blogfollow.model.BlogFollowEvent
 import linkletter.client.feature.blogfollow.model.BlogFollowMessage
 import linkletter.client.feature.blogfollow.model.BlogFollowState
-import linkletter.client.feature.blogfollow.model.BlogFollowUiModel
 
-class BlogFollowViewModel : ViewModel() {
-    private val _state = MutableStateFlow<BlogFollowState>(BlogFollowState())
+class BlogFollowViewModel(
+    private val getBlogUseCase: GetBlogUseCase,
+) : ViewModel() {
+    private val _state = MutableStateFlow<BlogFollowState>(BlogFollowState.Empty)
     val state = _state.asStateFlow()
 
     private val _effect = Channel<BlogFollowEffect>(Channel.BUFFERED)
     val effect get() = _effect.receiveAsFlow()
 
-    init {
-        fetchBlogList()
-    }
-
     fun onEvent(event: BlogFollowEvent) {
         when (event) {
+            is BlogFollowEvent.Search -> fetchBlogFollow(url = event.query)
             is BlogFollowEvent.BackClicked -> back()
             is BlogFollowEvent.BlogClicked -> openUri(link = event.link)
-            is BlogFollowEvent.BlogFollowToggleClicked -> toggleBlogFollow(blogFollowUiModel = event.blogFollowUiModel)
+            is BlogFollowEvent.BlogFollowToggleClicked -> toggleBlogFollow()
         }
     }
 
-    private fun fetchBlogList() {
-        _state.value = BlogFollowState.Dummy
+    private fun fetchBlogFollow(url: String) {
+        getBlogUseCase(url = url)
+            .onStart {
+                _state.value = BlogFollowState.Loading
+                delay(1000)
+            }.onEach { blog ->
+                val postList =
+                    blog.postList.map { post ->
+                        post.copy(pubDate = post.pubDate.formatRssDateToKorean())
+                    }
+                val newState =
+                    BlogFollowState.BlogFollow(
+                        blog = blog.copy(postList = postList),
+                        isFollowed = false,
+                    )
+                _state.value = newState
+            }.catch { error ->
+                _state.value = BlogFollowState.Empty
+                viewModelScope.launch {
+                    _effect.send(
+                        BlogFollowEffect.ShowMessage(
+                            BlogFollowMessage.BlogNotFound,
+                        ),
+                    )
+                }
+            }.launchIn(viewModelScope)
     }
 
     private fun back() {
@@ -48,21 +77,32 @@ class BlogFollowViewModel : ViewModel() {
         }
     }
 
-    private fun toggleBlogFollow(blogFollowUiModel: BlogFollowUiModel) {
+    private fun toggleBlogFollow() {
         viewModelScope.launch {
-            val blogs =
-                state.value.blogFollowUiModelList.map {
-                    if (it.id == blogFollowUiModel.id) {
-                        it.copy(isFollowed = !it.isFollowed)
-                    } else {
-                        it
-                    }
+            if (_state.value is BlogFollowState.BlogFollow) {
+                val currentState = _state.value as BlogFollowState.BlogFollow
+                val newIsFollowed = !currentState.isFollowed
+
+                if (newIsFollowed) {
+                    _effect.send(
+                        BlogFollowEffect.ShowMessage(
+                            BlogFollowMessage.BlogFollowed(
+                                blogName = currentState.blog.name,
+                            ),
+                        ),
+                    )
+                } else {
+                    _effect.send(
+                        BlogFollowEffect.ShowMessage(
+                            BlogFollowMessage.BlogUnfollowed(
+                                blogName = currentState.blog.name,
+                            ),
+                        ),
+                    )
                 }
 
-            if (!blogFollowUiModel.isFollowed) {
-                _effect.send(BlogFollowEffect.ShowMessage(BlogFollowMessage.BlogFollowed(blogName = blogFollowUiModel.name)))
+                _state.value = currentState.copy(isFollowed = newIsFollowed)
             }
-            _state.value = state.value.copy(blogFollowUiModelList = blogs)
         }
     }
 }
---
+++ feature/blogfollow/src/commonMain/kotlin/linkletter/client/feature/blogfollow/components/BlogResultCard.kt
@@ -0,0 +1,106 @@
+package linkletter.client.feature.blogfollow.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import linkletter.client.core.designsystem.components.NetworkImage
+import linkletter.client.core.designsystem.theme.Gray500
+import linkletter.client.core.designsystem.theme.LinkletterTheme
+import linkletter.client.feature.blogfollow.model.BlogFollowState.BlogFollow
+
+@Composable
+internal fun BlogResultCard(
+    blogFollow: BlogFollow,
+    showPlaceholder: Boolean,
+    modifier: Modifier = Modifier,
+    onBlogClick: (String) -> Unit,
+    onBlogFollow: () -> Unit,
+) {
+    val shimmerModifier =
+        if (showPlaceholder) {
+            Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .width(200.dp)
+                .background(color = LinkletterTheme.colorScheme.placeholderColor)
+        } else {
+            Modifier
+        }
+    Surface(
+        modifier = modifier.padding(horizontal = 16.dp),
+        color = LinkletterTheme.colorScheme.surface,
+        shape = RoundedCornerShape(12.dp),
+        shadowElevation = 2.dp,
+    ) {
+        Row(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .clickable { onBlogClick(blogFollow.blog.url) }
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            NetworkImage(
+                imageUrl = blogFollow.blog.author.imageUrl,
+                contentDescription = "블로그 작성자 프로필 이미지",
+                modifier =
+                    Modifier
+                        .size(48.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .then(shimmerModifier),
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = blogFollow.blog.author.name,
+                    style = LinkletterTheme.typography.titleMediumR.copy(color = LinkletterTheme.colorScheme.onSurface),
+                    modifier = Modifier.then(shimmerModifier),
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = blogFollow.blog.url,
+                    style = LinkletterTheme.typography.titleSmallR.copy(color = Gray500),
+                    modifier = Modifier.then(shimmerModifier),
+                )
+            }
+            IconButton(
+                onClick = onBlogFollow,
+            ) {
+                Icon(
+                    imageVector = if (blogFollow.isFollowed) Icons.Default.Check else Icons.Default.Add,
+                    tint =
+                        if (blogFollow.isFollowed) {
+                            LinkletterTheme.colorScheme.primarySurface
+                        } else {
+                            LinkletterTheme.colorScheme.onSurface
+                        },
+                    contentDescription = if (blogFollow.isFollowed) "언팔로우" else "팔로우",
+                )
+            }
+        }
+    }
+}
