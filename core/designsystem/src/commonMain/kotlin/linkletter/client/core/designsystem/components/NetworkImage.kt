b7e59989a124b1ce44565263d13c8bbdfe13a1ca
JunJangE <wnswkd486@gmail.com>
2025-07-03 23:17:51 +0900

    feat: NetworkImage 기본 이미지 추가 (#21)

--- core/designsystem/src/commonMain/kotlin/linkletter/client/core/designsystem/components/NetworkImage.kt
+++ core/designsystem/src/commonMain/kotlin/linkletter/client/core/designsystem/components/NetworkImage.kt
@@ -1,5 +1,6 @@
 package linkletter.client.core.designsystem.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -8,6 +9,9 @@ import coil3.compose.AsyncImage
 import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import linkletter_client.core.designsystem.generated.resources.Res
+import linkletter_client.core.designsystem.generated.resources.ic_blue_owl
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun NetworkImage(
@@ -17,16 +21,25 @@ fun NetworkImage(
     contentScale: ContentScale = ContentScale.Crop,
     contentDescription: String? = null,
 ) {
-    AsyncImage(
-        model =
-            ImageRequest
-                .Builder(LocalPlatformContext.current)
-                .data(imageUrl)
-                .crossfade(true)
-                .build(),
-        modifier = modifier,
-        placeholder = placeholder,
-        contentScale = contentScale,
-        contentDescription = contentDescription,
-    )
+    if (imageUrl == null) {
+        Image(
+            painter = painterResource(Res.drawable.ic_blue_owl),
+            modifier = modifier,
+            contentScale = contentScale,
+            contentDescription = contentDescription,
+        )
+    } else {
+        AsyncImage(
+            model =
+                ImageRequest
+                    .Builder(LocalPlatformContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+            modifier = modifier,
+            placeholder = placeholder,
+            contentScale = contentScale,
+            contentDescription = contentDescription,
+        )
+    }
 }
