d359ff73b72254e734f91b51343f992ad9e5b7b9
JunJangE <wnswkd486@gmail.com>
2025-07-15 15:06:34 +0900

    feat: 권한 관련 로직 구현 (#37)

--- feature/setting/src/commonMain/kotlin/linkletter/client/feature/setting/SettingScreen.kt
+++ feature/setting/src/commonMain/kotlin/linkletter/client/feature/setting/SettingScreen.kt
@@ -6,11 +6,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dev.icerock.moko.permissions.Permission
+import dev.icerock.moko.permissions.PermissionState
+import dev.icerock.moko.permissions.PermissionsController
+import dev.icerock.moko.permissions.compose.BindEffect
+import dev.icerock.moko.permissions.compose.PermissionsControllerFactory
+import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import linkletter.client.core.designsystem.components.LinkletterTopAppBar
 import linkletter.client.core.designsystem.theme.LinkletterTheme
 import linkletter.client.feature.setting.components.AlarmToggle
@@ -18,6 +34,8 @@ import linkletter.client.feature.setting.model.SettingEvent
 import linkletter_client.feature.setting.generated.resources.Res
 import linkletter_client.feature.setting.generated.resources.alarm_toggle_description
 import linkletter_client.feature.setting.generated.resources.alarm_toggle_title
+import linkletter_client.feature.setting.generated.resources.snackbar_notification_permission_denied
+import linkletter_client.feature.setting.generated.resources.snackbar_notification_permission_denied_action
 import linkletter_client.feature.setting.generated.resources.title
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
@@ -30,6 +48,26 @@ internal fun SettingScreen(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
+    val factory: PermissionsControllerFactory = rememberPermissionsControllerFactory()
+    val controller: PermissionsController =
+        remember(factory) { factory.createPermissionsController() }
+    BindEffect(controller)
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        val permissionState = controller.getPermissionState(Permission.REMOTE_NOTIFICATION)
+        if (permissionState != PermissionState.Granted) {
+            viewModel.onEvent(SettingEvent.AlarmToggle(false))
+        }
+    }
+
+    val notificationPermissionDenied =
+        stringResource(Res.string.snackbar_notification_permission_denied)
+    val notificationPermissionDeniedAction =
+        stringResource(Res.string.snackbar_notification_permission_denied_action)
+
     Scaffold(
         containerColor = LinkletterTheme.colorScheme.background,
         modifier =
@@ -40,6 +78,16 @@ internal fun SettingScreen(
         topBar = {
             LinkletterTopAppBar(title = stringResource(Res.string.title))
         },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { data ->
+                Snackbar(
+                    snackbarData = data,
+                    containerColor = LinkletterTheme.colorScheme.onSurface,
+                    contentColor = LinkletterTheme.colorScheme.surface,
+                    actionColor = LinkletterTheme.colorScheme.primary,
+                )
+            }
+        },
     ) { innerPadding ->
         SettingContent(
             enabled = state.alarmEnabled,
@@ -47,7 +95,30 @@ internal fun SettingScreen(
                 Modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-            onToggle = { boolean -> viewModel.onEvent(SettingEvent.AlarmToggle(boolean)) },
+            onToggle = { shouldEnable ->
+                coroutineScope.launch {
+                    if (!shouldEnable) {
+                        viewModel.onEvent(SettingEvent.AlarmToggle(false))
+                        return@launch
+                    }
+                    runCatching {
+                        controller.providePermission(Permission.REMOTE_NOTIFICATION)
+                    }.onSuccess {
+                        viewModel.onEvent(SettingEvent.AlarmToggle(true))
+                    }.onFailure {
+                        val result =
+                            snackbarHostState.showSnackbar(
+                                message = notificationPermissionDenied,
+                                actionLabel = notificationPermissionDeniedAction,
+                                duration = SnackbarDuration.Short,
+                            )
+
+                        if (result == SnackbarResult.ActionPerformed) {
+                            controller.openAppSettings()
+                        }
+                    }
+                }
+            },
         )
     }
 }
